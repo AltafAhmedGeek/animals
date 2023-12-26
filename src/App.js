9d9371ae7896ca2237e2f918c7f9b109b70f1252
altaf <altafgeek@gmail.com>
2023-12-26 23:37:58 +0530

    feat: used useState Hook for basic Data manupulation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import AnimalShow from "./AnimalShow";
+import { useState } from "react";
 function App() {
+  const handleClick = () => {
+    setCount(count + 1)
+    console.log(count);
+  };
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleClick}>Add Animal</button>
+      <AnimalShow/>
+    </>
   );
 }
 
