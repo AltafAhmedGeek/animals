19d6b6d43f12deedb49bb826ce5d41eea197b6b4
altaf <altafgeek@gmail.com>
2024-01-05 23:52:00 +0530

    feat: added some images and changes in Animal Component

--- src/AnimalShow.jsx
+++ src/AnimalShow.jsx
@@ -1,7 +1,40 @@
-const AnimalShow = () => {
-  return (
-    <div>AnimalShow</div>
-  )
-}
+import { useState } from "react";
+import './css/AnimalShow.css'
+import bird from "./images/bird.svg";
+import cat from "./images/cat.svg";
+import cow from "./images/cow.svg";
+import dog from "./images/dog.svg";
+import gator from "./images/gator.svg";
+import heart from "./images/heart.svg";
+import horse from "./images/horse.svg";
 
-export default AnimalShow
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  heart,
+  horse,
+};
+const AnimalShow = ({ type }) => {
+  const [click, setClick] = useState(0);
+  const handleClick = () => {
+    setClick(click + 1);
+  };
+  return (
+    <div className="animal-show">
+      <img className='animal' src={svgMap[type]} alt="animal" onClick={handleClick} />
+      <img
+        className="heart"
+        src={heart}
+        alt="heart"
+        style={{
+          width: 10 + 10 * click + "px",
+        }}
+      />
+    </div>
+  );
+};
+
+export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import AnimalShow from "./AnimalShow";
 import { useState } from "react";
+import "./css/App.css";
+function getRandomAnimal() {
+  const animals = ["cat", "bird", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 function App() {
+  const [animals, setAnimals] = useState([]);
   const handleClick = () => {
-    setCount(count + 1)
-    console.log(count);
+    setAnimals([...animals, getRandomAnimal()]);
+    console.log(animals);
   };
-  const [count, setCount] = useState(0);
+  const renderedAnimals = animals.map((animal, i) => {
+    return <AnimalShow key={i} type={animal} />;
+  });
+
   return (
-    <>
-      <button onClick={handleClick}>Add Animal</button>
-      <AnimalShow/>
-    </>
+    <div className="app">
+      <button className="button" onClick={handleClick}>
+        Add Animal
+      </button>
+      <div className="animal-list">{renderedAnimals}</div>
+    </div>
   );
 }
 
